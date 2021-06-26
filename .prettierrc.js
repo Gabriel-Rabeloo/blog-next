4e144c3bfd24636c4327c7907789c3569d8a3ed3
Gabriel Rabelo <gabriel872003@gmail.com>
2021-06-26 19:32:39 -0300

    Organização das pastas do projeto

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   semi: true,
   trailingComma: 'all',
-  sigleQuote: true,
+  singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
 }
---
+++ src/config/app-config.ts
@@ -0,0 +1,5 @@
+export const API_URL = 'https://desolate-stream-36742.herokuapp.com';
+export const POSTS_URL = `${API_URL}/posts`;
+
+export const SITE_NAME = 'Gabriel Rabelo';
+export const SITE_URL = '';
