7c484a978a2d7346f90b2be6982375014286e7ff
Gabriel Rabelo <gabriel872003@gmail.com>
2021-06-30 21:42:34 -0300

    Adiocionando url de produção

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,5 @@ export const API_URL = 'https://desolate-stream-36742.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Gabriel Rabelo';
-export const SITE_URL = process.env.SITE_URL || 'http:127.0.0.1:3000';
+export const SITE_URL =
+  process.env.SITE_URL || 'https://gabriel-blog-next.netlify.app';
